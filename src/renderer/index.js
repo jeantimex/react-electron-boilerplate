c756817784f7e2cd062cc9d3f1242c9b4d16340a
jeantimex <jean.timex@gmail.com>
2017-06-08 21:48:00 -0700

    Added todo list app.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,22 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import App from 'components/App';
+// Redux and React Router
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import store from 'store';
+
+// App components
+//import App from 'components/app';
+import Todos from 'components/todos/Todos';
+
+const history = createHistory();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Todos />
+  </Provider>,
   document.getElementById('root')
 );
